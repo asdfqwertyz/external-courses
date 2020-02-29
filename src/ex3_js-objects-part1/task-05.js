8328f0f9cb9dc44cc252c692ca15ca39c88894cc
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-02-29 19:40:05 +0300

    Update task-05.js

--- src/ex3_js-objects-part1/task-05.js
+++ src/ex3_js-objects-part1/task-05.js
@@ -1,11 +1,11 @@
 'use strict'
 function getCloneObj(obj)
 {
-	let cloneObj = {}
-	for (let key in obj)
-	{
-	cloneObj[key] = obj[key]
-	}
+  let cloneObj = {}
+  for (let key in obj)
+  {
+    cloneObj[key] = obj[key]
+  }
 return cloneObj
 }
 module.exports = getCloneObj

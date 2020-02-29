79f7fe2247fbbf0086c26282da72cc5a15b0487e
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-02-29 19:44:47 +0300

    Updated task-01.js

--- src/ex2_js-basics-part2/task-01.js
+++ src/ex2_js-basics-part2/task-01.js
@@ -1,19 +1,18 @@
 'use strict'
 function getType(arg)
 {
-	if (typeof(arg) === 'number')
-	{  
-		if (isNaN(arg) === true)
-		{
-			return NaN
-		}
-			return 'number'
-		
-	}
-	else if (typeof(arg) === 'string')
-	{
-		return 'string'
-	}
-		return undefined
+  if (typeof(arg) === 'number')
+  {  
+    if (isNaN(arg) === true)
+    {
+      return NaN
+    }
+    return 'number'
+  }
+  else if (typeof(arg) === 'string')
+  {
+    return 'string'
+  }
+return undefined
 }
 module.exports = getType

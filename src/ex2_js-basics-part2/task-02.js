b61196ff249115f6bfeb06f61912846dbf42203a
asdfqwertyz <maxiliuk1@mail.ru>
2020-02-29 20:06:09 +0300

    merge

---
+++ src/ex2_js-basics-part2/task-02.js
@@ -0,0 +1,10 @@
+'use strict'
+function getInfo(arr)
+{
+  for (let i = 0; i < arr.length; i++) 
+  {
+    console.log(arr[i])
+  }
+  console.log(`Длина массива = ${arr.length}`)
+}
+module.exports = getInfo

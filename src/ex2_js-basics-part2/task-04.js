5cb4002158f940146364cc18dcbfb6924ff9bc17
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-02-29 19:46:50 +0300

    Updated task-04.js

--- src/ex2_js-basics-part2/task-04.js
+++ src/ex2_js-basics-part2/task-04.js
@@ -1,18 +1,18 @@
 'use strict'
 function isSameArray(arr)
 {
-	let numOfRepeat = 1;
-	for (let i = 0;i<arr.length;i++)
-	{
-		if (arr[i] === arr[i+1])
-		{
-			numOfRepeat++
-		}
-	}
-	if (numOfRepeat === arr.length)
-	{
-		return true
-	}
-	return false
+  let numOfRepeat = 1;
+  for (let i = 0;i<arr.length;i++)
+  {
+    if (arr[i] === arr[i+1])
+    {
+      numOfRepeat++
+    }
+  }
+  if (numOfRepeat === arr.length)
+  {
+    return true
+  }
+return false
 }
 module.exports = isSameArray

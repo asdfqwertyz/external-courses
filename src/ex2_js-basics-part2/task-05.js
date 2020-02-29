86176386bc26c9bb6caeec7cbc40e2437c2c20f1
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-02-29 19:47:14 +0300

    Updated task-05.js

--- src/ex2_js-basics-part2/task-05.js
+++ src/ex2_js-basics-part2/task-05.js
@@ -1,14 +1,14 @@
 'use strict'
 function getMaxDigit(arr)
 {
-	let MaxDigitArray = 0
-	for (let i = 0; i<arr.length; i++) 
-	{
-		if (MaxDigitArray<arr[i])
-		{
-			MaxDigitArray=arr[i]
-		}
-	}
-	return MaxDigitArray
+  let MaxDigitArray = 0
+  for (let i = 0; i<arr.length; i++) 
+  {
+    if (MaxDigitArray<arr[i])
+    {
+      MaxDigitArray=arr[i]
+    }
+  }
+return MaxDigitArray
 }
 module.exports = getMaxDigit

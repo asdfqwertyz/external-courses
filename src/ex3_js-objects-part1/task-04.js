6882d9c4d53a9cb535f232e300e0c60886b0d0ef
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-02-29 19:39:36 +0300

    Update task-04.js

--- src/ex3_js-objects-part1/task-04.js
+++ src/ex3_js-objects-part1/task-04.js
@@ -1,15 +1,15 @@
 'use strict'
 function showObj(str,obj)
 {
-	if (obj.hasOwnProperty(str) !== true)
-	{
-        obj[str] = "new" //eslint-disable-line
-	}
-	let objClone = {}
-	for (let key in obj)
-	{
-	objClone[key] = obj[key]
-	}
+  if (obj.hasOwnProperty(str) !== true)
+  {
+    obj[str] = "new" //eslint-disable-line
+  }
+  let objClone = {}
+  for (let key in obj)
+  {
+    objClone[key] = obj[key]
+  }
 return obj
 }
 module.exports = showObj

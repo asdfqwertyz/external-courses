7a1cceb69614f02b378f5de220096d99a62e1d80
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-03-13 15:48:19 +0300

    Fixed task-01.js

--- src/ex2_js-basics-part2/task-01.js
+++ src/ex2_js-basics-part2/task-01.js
@@ -3,7 +3,7 @@ function getType(arg){
 	if (typeof(arg) === 'number' && Number.isNaN(arg) === false){  
 			return 'number'
 	}
-	else if (typeof(arg) === 'string'){
+	else if (typeof(arg) === "string"){
 		return "string"
 	}
 		return undefined

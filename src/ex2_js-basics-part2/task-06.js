981444ea5f28ca870d486cffb2a21a3a43a3d91d
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-03-13 15:50:19 +0300

    Fixed task-06.js

--- src/ex2_js-basics-part2/task-06.js
+++ src/ex2_js-basics-part2/task-06.js
@@ -1,26 +1,19 @@
-function isPrime(num)
-{
-	if (num >1000 || num<0)
-	{
+function isPrime(num){
+	if (num >1000 || num<0){
 		return "Данные неверны"
 	}
-	if (num === 1)
-	{
+	if (num === 1){
 		return `Число ${num} - составное число`
 	}
-	if (num ===2)
-	{
+	if (num ===2){
 		return `Число ${num} - простое число`
 	}
-	if (num === 0)
-	{
+	if (num === 0){
 		return "Число ни простое ни составное"
 	}
 	
-	for (let i = 2; i<num;i++)
-	{
-		if (num % i === 0)
-		{
+	for (let i = 2; i<num;i++){
+		if (num % i === 0){
 			return `Число ${num} - составное число`
 		}
 	}

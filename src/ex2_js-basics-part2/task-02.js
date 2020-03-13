7154d1a456613d859a115256df592b5f7085e2e7
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-03-13 15:32:30 +0300

    Fixed task-02.js

--- src/ex2_js-basics-part2/task-02.js
+++ src/ex2_js-basics-part2/task-02.js
@@ -1,8 +1,6 @@
 'use strict'
-function getInfo(arr)
-{
-	for (let i = 0; i < arr.length; i++) 
-	{
+function getInfo(arr){
+	for (let i = 0; i < arr.length; i++) {
 		console.log(arr[i])
 	}
 	console.log("Длина массива = ${arr.length}")

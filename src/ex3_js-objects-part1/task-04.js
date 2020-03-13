061e0c1ca0d5c8c9e5f9ea2cb4e176eafbf9421f
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-03-13 18:18:44 +0300

    Fixed task-04.js

--- src/ex3_js-objects-part1/task-04.js
+++ src/ex3_js-objects-part1/task-04.js
@@ -2,7 +2,7 @@
 function showObj(str,obj){
 	let objClone = obj
   if (obj.hasOwnProperty(str) !== true){
-    objClone[str] = "new" 
+    objClone[str] = 'new'
   }
   for (let key in obj){
     objClone[key] = obj[key]

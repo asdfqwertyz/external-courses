0a3314ffb38086fee40397536c66974865a50248
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-03-13 15:47:39 +0300

    Fixed task-03.js

--- src/ex2_js-basics-part2/task-03.js
+++ src/ex2_js-basics-part2/task-03.js
@@ -1,28 +1,17 @@
 'use strict'
-function getQuantity(arr)
-{
+function getQuantity(arr){
 	let even = 0
-	let odd = 0
 	let zero = 0
-	for (let i = 0; i < arr.length; i++) 
-	{
-		if (arr[i] % 2 === 0 && arr[i] !== null)
-		{
-			if (arr[i] === 0)
-			{
+	for (let i = 0; i < arr.length; i++){
+		if (arr[i] % 2 === 0 && arr[i] !== null){
+			if (arr[i] === 0){
 				zero+=1
 			}
-			else
-			{
+			else{
 			even += 1
 			}
 		}
-		else if (arr[i] % 2 === 1 || arr[i] % 2 === -1)
-		{
-			odd += 1
-		}
-
 	}
-	return [even, odd, zero]
+	return [even, arr.length-zero-even, zero]
 }
 module.exports = getQuantity

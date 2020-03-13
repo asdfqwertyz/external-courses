2a80ffcad79f6868aea7248dc9e4e4b2b42975eb
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-03-13 15:41:50 +0300

    Fixed task-06.js

--- src/ex3_js-objects-part1/task-06.js
+++ src/ex3_js-objects-part1/task-06.js
@@ -1,19 +1,14 @@
 'use strict'
-function getDeepCopyObj(obj) 
-{
+function getDeepCopyObj(obj) {
   let cloneObj = {};
-  for (let key in obj)
-  {
+  for (let key in obj){
     cloneObj[key] = obj[key];
-    if (typeof obj[key] === "object") 
-    {
-      if (Array.isArray(obj[key]) === true) 
-      {
+    if (typeof obj[key] === "object") {
+      if (Array.isArray(obj[key]) === true) {
         let objKey = getDeepCopyObj(obj[key])
         cloneObj[key] = [objKey[0]];
       }
-      else
-      {
+      else{
         let objKey = getDeepCopyObj(obj[key]);
         cloneObj[key] = objKey;
       }

7b388922fd15b98f51a13267e64b87d57c4a3dc7
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-03-18 19:51:18 +0300

    Update task-03.js

--- src/ex4_js-objects-part2/task-03.js
+++ src/ex4_js-objects-part2/task-03.js
@@ -1,9 +1,7 @@
 'use strict'
-function showString(str)
-{
+function showString(str){
   let strRez
-  if (str.indexOf(' ') === 0 && str.lastIndexOf(' ') === str.length-1)
-  {
+  if (str.indexOf(' ') === 0 && str.lastIndexOf(' ') === str.length-1){
     strRez = str.slice(1,str.length-1)
     return strRez
   }

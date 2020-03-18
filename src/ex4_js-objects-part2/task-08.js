b788a5eeedbf455f97eb820c7620958a361488ab
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-03-18 19:49:48 +0300

    Update task-08.js

--- src/ex4_js-objects-part2/task-08.js
+++ src/ex4_js-objects-part2/task-08.js
@@ -1,19 +1,14 @@
 'use strict'
-function getLowerCamelCase(str)
-{
+function getLowerCamelCase(str){
   let newStr = ''
   let strArr
   strArr = str.toLowerCase().split(' ')
-  for (let i = 0; i<strArr.length; i++)
-  {
-    if (strArr[i] !== '')
-    {
-      if (i === 0)
-      {
+  for (let i = 0; i<strArr.length; i++){
+    if (strArr[i] !== ''){
+      if (i === 0){
         newStr+=strArr[i][0].toLowerCase() + strArr[i].substring(1) 
       }
-      else
-      {
+      else{
         newStr+=strArr[i][0].toUpperCase() + strArr[i].substring(1) 
       }
     }

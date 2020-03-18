a8ee196acce35be098c60a5916e75226764c6656
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-03-18 19:46:10 +0300

    Update task-01.js

--- src/ex4_js-objects-part2/task-01.js
+++ src/ex4_js-objects-part2/task-01.js
@@ -1,8 +1,6 @@
 'use strict'
-function findProperty(prop,obj)  //eslint-disable-line
-{
-	if (obj.hasOwnProperty(prop) === false)
-    {
+function findProperty(prop,obj)  //eslint-disable-line{
+	if (obj.hasOwnProperty(prop) === false){
       return obj[prop]
     }
 }

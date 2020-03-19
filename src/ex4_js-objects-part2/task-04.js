3e0511cc3d73942272a72bc41138abb087b1650b
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-03-19 21:04:35 +0300

    Update task-04.js

--- src/ex4_js-objects-part2/task-04.js
+++ src/ex4_js-objects-part2/task-04.js
@@ -1,7 +1,5 @@
 'use strict'
 function showString(str){
-  let upperChar = str[0].toUpperCase()
-  let newStr = str.slice(1)
-  return upperChar+newStr
+  return str[0].toUpperCase()+str.slice(1)
 }
 module.exports = showString

7ef3b4248c8d0f796fe4feb47802f61ac7d3efd9
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-03-19 21:41:27 +0300

    Update task-10.js

--- src/ex4_js-objects-part2/task-10.js
+++ src/ex4_js-objects-part2/task-10.js
@@ -1,9 +1,6 @@
 'use strict'
 function getReverse(str){
-  let reverseStr = []
-  for (let i = 1;i<=str.length;i++){
-    reverseStr[i] = str[str.length-i]
-  }
-  return(reverseStr.join(''))
+  let newStr = str.split('').reverse().join('')
+  return newStr
 }
 module.exports = getReverse

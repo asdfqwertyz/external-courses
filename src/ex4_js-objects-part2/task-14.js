87949ce7a309942f085193e3c1d4cc155bc5d33c
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-03-20 20:44:39 +0300

    Update task-14.js

--- src/ex4_js-objects-part2/task-14.js
+++ src/ex4_js-objects-part2/task-14.js
@@ -1,5 +1,5 @@
 'use strict'
-function getRandom(min,max){ 
-   return Math.round(Math.random()*(max-min))+min 
+function getRandom(min, max) { 
+   return Math.round(Math.random()*(max - min)) + min 
 }
 module.exports = getRandom

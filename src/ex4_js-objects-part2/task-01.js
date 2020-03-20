4baba53cd7efa2f03c027324a5117b6e3f5e5019
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-03-20 20:40:24 +0300

    Update task-01.js

--- src/ex4_js-objects-part2/task-01.js
+++ src/ex4_js-objects-part2/task-01.js
@@ -1,6 +1,6 @@
 'use strict'
-function findProperty(prop, obj){
-  if (!obj.hasOwnProperty(prop)){
+function findProperty(prop, obj) {
+  if (!obj.hasOwnProperty(prop)) {
     return obj[prop]
   }
   return undefined

d72f5deb4a8e2777e195fdc3ec4acd2e197fe627
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-03-20 20:43:53 +0300

    Update task-11.js

--- src/ex4_js-objects-part2/task-11.js
+++ src/ex4_js-objects-part2/task-11.js
@@ -1,9 +1,9 @@
 'use strict'
 function getCountWords(str){
-  let newArr = []
+  let newArr = [];
   let countWords = 0;
-  for (let i = 0; i<str.length; i++){
-    if(newArr.includes(str.indexOf(str[i])) === false){
+  for (let i = 0; i < str.length; i++) {
+    if(newArr.includes(str.indexOf(str[i])) === false) {
       newArr.push(str.indexOf(str[i]))
       console.log(++countWords)
     }

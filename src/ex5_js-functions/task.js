ac33b87293fe90c25f5e8f7ca823f17f0154497e
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-03-20 14:17:04 +0300

    Update task.js

--- src/ex5_js-functions/task.js
+++ src/ex5_js-functions/task.js
@@ -1,56 +1,28 @@
 'use strict'
 const Calculator = {
-  curentNum:0,
-  add:function(a = 0){
-    Calculator.curentNum+= a;
-    function f(b = 0) {
-      Calculator.curentNum += b;
-      return f;
-    }
-    f.toString = function(){
-      return Calculator.curentNum;
-    };
-    return f;
+  curentNum: 0,
+  add: function (a = 0) {
+    this.curentNum += a;
+    return b => this.add(b);
   },
-  substract:function(a = 0){
-    Calculator.curentNum-= a;
-    function f(b = 0) {
-      Calculator.curentNum -= b;
-      return f;
-    }
-    f.toString = function(){
-      return Calculator.curentNum;
-    };
-    return f;
+  substact: function (a = 0) {
+    this.curentNum -= a;
+    return b => this.substact(b);
   },
-  divide:function(a = 0){
-    Calculator.curentNum/= a;
-    function f(b = 0) {
-      Calculator.curentNum /= b;
-      return f;
-    }
-    f.toString = function(){
-      return Calculator.curentNum;
-    };
-    return f;
+  divide: function (a = 0) {
+    this.curentNum /= a;
+    return b => this.divide(b);
   },
-  multiply:function(a = 0) {
-    Calculator.curentNum*= a;
-    function f(b = 0) {
-      Calculator.curentNum *= b;
-      return f;
-    }
-    f.toString = function(){
-      return Calculator.curentNum;
-    };
-    return f;
+  multiply: function (a = 0) {
+    this.curentNum *= a;
+    return b => this.multiply(b);
   },
   getResult:function(){
-    return Calculator.curentNum
+    return this.curentNum;
   },
   reset:function(){
-    Calculator.curentNum = 0
-    return Calculator.curentNum
+    this.curentNum = 0
+    return this.curentNum;
   }
 }
 module.exports(Calculator)

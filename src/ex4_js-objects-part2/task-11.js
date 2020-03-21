f100fad4a269bce65259f99692758530425149d6
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-03-22 00:29:47 +0300

    Update task-11.js

--- src/ex4_js-objects-part2/task-11.js
+++ src/ex4_js-objects-part2/task-11.js
@@ -1,12 +1,20 @@
 'use strict'
-function getCountWords(str){
+function getCountWords(str) {
   let newArr = [];
-  let countWords = 0;
+  let uniqueWords = [];
+  let countUniqueWords = -1;
   for (let i = 0; i < str.length; i++) {
-    if(newArr.includes(str.indexOf(str[i])) === false) {
-      newArr.push(str.indexOf(str[i]))
-      console.log(++countWords)
+    if(!newArr.includes(str[i])) {
+      countUniqueWords++;
+      uniqueWords[countUniqueWords] = 0;
+      newArr.push(str[i]);
+    } 
+    else {
+      uniqueWords[countUniqueWords]++;
     }
+  }
+  for (let i = 0; i < newArr.length; i++) {
+    console.log(`Символ ${newArr[i]} - ${uniqueWords[i]+1}`);
   }
 }
 module.exports = getCountWords

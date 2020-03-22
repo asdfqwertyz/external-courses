9dde9924989d226340004a32d26356740d38bc91
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-03-23 00:18:23 +0300

    Update task-09.js

--- src/ex4_js-objects-part2/task-09.js
+++ src/ex4_js-objects-part2/task-09.js
@@ -1,8 +1,10 @@
 'use strict'
-function showString(str,substr,num){
-  let newArr
-  newArr = str.split(' ')
-  newArr.splice(num+1,0,substr)
-  return newArr.join(' ')
+function showString(str, substr, num) {
+  let copyArr1;
+  let copyArr2;
+  copyArr1 = str.split(" ").slice(0, num + 1);
+  copyArr1.push(substr);
+  copyArr2 = str.split(" ").slice(num + 1, str.length);
+  return copyArr1.join(" ") + " " + copyArr2.join(" ");
 }
 module.exports = showString

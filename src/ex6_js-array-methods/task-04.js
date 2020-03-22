d741e5f40585af6ba0d31e889cdc0e12c02c5f4e
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-03-22 16:05:41 +0300

    Update task-04.js

--- src/ex6_js-array-methods/task-04.js
+++ src/ex6_js-array-methods/task-04.js
@@ -1,11 +1,9 @@
 'use strict'
 function filter(array, callback) {
   let arrTrueItems = [];
-  let item;
   for (let i = 0; i < array.length; i++) {
-    item = array[i];
-    if (callback(item, i, array) === true) {
-      arrTrueItems.push(item);
+    if (callback(array[i], i, array)) {
+      arrTrueItems.push(array[i]);
     }
   }
   return arrTrueItems;

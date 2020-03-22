e1d6a48586ac306ddca56cfce75f9e9051a34adf
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-03-22 15:57:35 +0300

    Update task-02.js

--- src/ex6_js-array-methods/task-02.js
+++ src/ex6_js-array-methods/task-02.js
@@ -1,13 +1,9 @@
 'use strict'
 function some(array, callback) {
-  let arrBool = [];
-  let item;
   for (let i = 0; i < array.length; i++) {
-    item = array[i];
-    arrBool.push(callback(item, i, array))
-  }
-  if (arrBool.includes(true)) {
-    return true;
+    if (callback(array[i], i, array)) {
+      return true;
+    }
   }
   return false;
 }

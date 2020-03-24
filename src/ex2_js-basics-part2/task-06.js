1f61eb782618ae8b93ebb66bfbcf94599b55fba4
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-03-24 13:28:57 +0300

    Update task-06.js

--- src/ex2_js-basics-part2/task-06.js
+++ src/ex2_js-basics-part2/task-06.js
@@ -1,22 +1,25 @@
-function isPrime(num){
-	if (num >1000 || num<0){
-		return "Данные неверны"
-	}
-	if (num === 1){
-		return `Число ${num} - составное число`
-	}
-	if (num ===2){
-		return `Число ${num} - простое число`
-	}
-	if (num === 0){
-		return "Число ни простое ни составное"
-	}
-	
-	for (let i = 2; i<num;i++){
-		if (num % i === 0){
-			return `Число ${num} - составное число`
-		}
-	}
-	return `Число ${num} - простое число`
+function isPrime(num) {
+  if (num > 1000) {
+    return "Данные неверны";
+  }
+  if (num < 0) {
+    return "Число отрицательное";
+  }
+  if (num === 1) {
+    return `Число ${num} - составное число`;
+  }
+  if (num === 2) {
+    return `Число ${num} - простое число`;
+  }
+  if (num === 0) {
+    return "Число ни простое ни составное";
+  }
+
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return `Число ${num} - составное число`;
+    }
+  }
+  return `Число ${num} - простое число`;
 }
 module.exports = isPrime

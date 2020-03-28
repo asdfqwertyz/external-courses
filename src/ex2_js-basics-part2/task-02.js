7a1739aa525ab692e60e6390f4ee2228a8a04ceb
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-03-28 15:47:01 +0300

    Update task-02.js

--- src/ex2_js-basics-part2/task-02.js
+++ src/ex2_js-basics-part2/task-02.js
@@ -1,8 +1,8 @@
-'use strict'
-function getInfo(arr){
-	for (let i = 0; i < arr.length; i++){
-		console.log(arr[i])
-	}
-	console.log("Длина массива = ${arr.length}")
+"use strict"
+function getInfo(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+  console.log("Длина массива = ${arr.length}");
 }
 module.exports = getInfo

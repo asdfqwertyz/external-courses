32a2463c0d65da7212feef862cef26b6002ed387
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-04-12 14:11:24 +0300

    Update task-01.js

--- src/ex12_js_dom/task-01.js
+++ src/ex12_js_dom/task-01.js
@@ -1,31 +1,23 @@
-const objectImages = {
-	'first':'url("assets/img1.jpg")',
-	2:'url("assets/img2.jpg")',
-	3:'url("assets/img3.png")',
-	'last':'url("assets/img4.jpg")'
-};
-let elem = document.getElementsByClassName('slider__img');
-elem[0].style.backgroundSize = "100% 100%";
-let i = 0;
-let arrImgs = [];
-for (let el in objectImages) {
-	arrImgs.push(el);
-}
-let btnNext = document.getElementsByClassName("button_next");
-btnNext[0].onclick = function() {
-	if (i >= arrImgs.length) {
+const arrayOfImages = ['url("assets/img1.jpg")','url("assets/img2.jpg")','url("assets/img3.png")','url("assets/img4.jpg")'];
+let slider = document.getElementsByClassName('slider__img')[0];
+slider.style.backgroundImage = 'url(assets/img2.jpg)'
+slider.style.backgroundSize = "100% 100%";
+let i = 2;
+let btnNext = document.getElementsByClassName("button_next")[0];
+btnNext.onclick = function() {
+	if (i >= arrayOfImages.length) {
 		i = 0;
 	}
 	i++;
-	elem[0].style.backgroundImage = "none";
-	elem[0].style.backgroundImage = objectImages[arrImgs[i-1]];
+	slider.style.backgroundImage = "none";
+	slider.style.backgroundImage = arrayOfImages[i-1];
 }
-let btnPrev = document.getElementsByClassName("button_prev");
-btnPrev[0].onclick = function() {
+let btnPrev = document.getElementsByClassName("button_prev")[0];
+btnPrev.onclick = function() {
 	if (i <= 1) {
-		i = arrImgs.length + 1;
+		i = arrayOfImages.length + 1;
 	}
 	i--;
-	elem[0].style.backgroundImage = "none";
-	elem[0].style.backgroundImage = objectImages[arrImgs[i-1]];
+	slider.style.backgroundImage = "none";
+	slider.style.backgroundImage = arrayOfImages[i-1];
 }

c8fe40d6fa0fefc36a6768c725be51628f3fa360
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-04-14 16:29:14 +0300

    Update main.js

--- src/kanban/main.js
+++ src/kanban/main.js
@@ -1,25 +1,24 @@
 const userMenu = document.getElementsByClassName('user-menu')[0];
+const upperCase = ['My account', 'My tasks', 'Log out'];
 let isOpenMenu = false;
 function openMenu() {
-    const arrayOfLinksName = ['My account', 'My tasks', 'Log out']
     const scrollMenu = document.createElement('div')
-    scrollMenu.setAttribute("class", "scroll-menu");
     const header = document.querySelector('.header');
-    const arrow = document.querySelector('.arrow')
-    for (let i = 0; i < arrayOfLinksName.length; i++)
-    {
-        scrollMenu.appendChild(document.createElement('a')).setAttribute("class", "menu-item")
-        scrollMenu.childNodes[i].setAttribute("href", "#")
-        scrollMenu.childNodes[i].innerHTML = arrayOfLinksName[i];
-    }
-    arrow.style.transform = "rotate(180deg)"
+    const arrow = document.querySelector('.arrow');
+ 		arrow.style.transform = "rotate(180deg)";
     if (isOpenMenu === true) {
         document.getElementsByClassName('scroll-menu')[0].remove();
         isOpenMenu = false;
-        arrow.style.transform = "rotate(0deg)"
+        arrow.style.transform = "rotate(0deg)";
     } else {
-       header.append(scrollMenu);
+       userMenu.append(scrollMenu);
        isOpenMenu = true;
-    }
-}
+    };
+    scrollMenu.setAttribute("class", "scroll-menu")
+    for (let i = 0; i < upperCase.length; i++) {
+    	 scrollMenu.appendChild(document.createElement('a')).setAttribute("class", "menu-item")
+       scrollMenu.childNodes[i].setAttribute("href", "#")
+       scrollMenu.childNodes[i].innerHTML = upperCase[i];
+    };
+};
 userMenu.addEventListener("click", openMenu)

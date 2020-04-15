afd58b1925e4a93bdc9894fbe57351a72898065a
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-04-15 22:16:14 +0300

    Update task-01.css

--- src/ex12_js_dom/task-01.js
+++ src/ex12_js_dom/task-01.js
@@ -1,6 +1,6 @@
 const images = ["assets/img1.jpg", "assets/img2.jpg", "assets/img3.png", "assets/img4.jpg"];
 const slider = document.getElementById('slider__img');
-slider.setAttribute("style", `background-image: url('${images[0]}')`)
+slider.setAttribute("style", `background-image: url('${images[0]}')`);
 let index = 2;
 const btnNext = document.getElementsByClassName("button_next")[0];
 const btnPrev = document.getElementsByClassName("button_prev")[0];

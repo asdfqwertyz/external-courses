98bc399716cf505467d9c0a4aa735d1e61e62591
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-04-15 21:56:36 +0300

    Update task-01.js

--- src/ex12_js_dom/task-01.js
+++ src/ex12_js_dom/task-01.js
@@ -1,21 +1,22 @@
-const arrayOfImages = ['url("assets/img1.jpg")','url("assets/img2.jpg")','url("assets/img3.png")','url("assets/img4.jpg")'];
-const slider = document.getElementsByClassName('slider__img')[0];
-let i = 2;
+const images = ["assets/img1.jpg", "assets/img2.jpg", "assets/img3.png", "assets/img4.jpg"];
+const slider = document.getElementById('slider__img');
+slider.style.backgroundImage = `url('${images[0]}')`
+let index = 2;
 const btnNext = document.getElementsByClassName("button_next")[0];
 const btnNextClick = function() {
-	if (i >= arrayOfImages.length) {
-		i = 0;
+	if (index >= images.length) {
+		index = 0;
 	}
-	i++;
-	slider.style.backgroundImage = arrayOfImages[i-1];
+	index++;
+	slider.style.backgroundImage = `url('${images[index-1]}')`;
 }
 const btnPrev = document.getElementsByClassName("button_prev")[0];
 const btnPrevClick = function() {
-	if (i <= 1) {
-		i = arrayOfImages.length + 1;
+	if (index <= 1) {
+		index = images.length + 1;
 	}
-	i--;
-	slider.style.backgroundImage = arrayOfImages[i-1];
+	index--;
+	slider.style.backgroundImage = `url('${images[index-1]}')`;
 }
 btnNext.addEventListener('click', btnNextClick);
 btnPrev.addEventListener('click', btnPrevClick);

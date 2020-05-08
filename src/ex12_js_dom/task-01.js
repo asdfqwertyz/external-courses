bbc12dd290ae5483f8cb4fe98bb852d8925bf507
asdfqwertyz <61510288+asdfqwertyz@users.noreply.github.com>
2020-05-08 18:04:04 +0300

    Update task-01.js

--- src/ex12_js_dom/task-01.js
+++ src/ex12_js_dom/task-01.js
@@ -1,37 +1,40 @@
 const images = ["assets/img1.jpg", "assets/img2.jpg", "assets/img3.png", "assets/img4.jpg"];
 const bgImage = document.getElementsByClassName('bgImage')[0];
-const leftArrow = 37;
-const rightArrow = 39;
-let index = 1;
-const btnNext = document.getElementsByClassName("button_next")[0];
-const btnPrev = document.getElementsByClassName("button_prev")[0];
+const LEFT_ARROW_KEY = 37;
+const RIGHT_ARROW_KEY = 39;
+let currentIndexImage = 1;
+const btnNext = document.querySelector('.button_next')
+const btnPrev = document.querySelector('.button_prev')
 const btnNextClick = () => {
-	if (index >= images.length) {
-		index = 0;
+	if (currentIndexImage >= images.length) {
+		currentIndexImage = 0;
 	}
-	index++;
-	bgImage.src = `${images[index-1]}`;
-	bgImage.classList.add('bgImage-animation');
-	setTimeout(() => bgImage.classList.remove('bgImage-animation'),1000);
+	currentIndexImage++;
+	bgImage.src = `${images[currentIndexImage-1]}`;
+	bgImage.classList.add('bgImage-animation')
+	setTimeout(() => bgImage.classList.remove('bgImage-animation'),500)
 }
 const btnPrevClick = () => {
-	if (index <= 1) {
-		index = images.length + 1;
-	};
-	index--;
-	bgImage.src = `${images[index-1]}`;
-	bgImage.classList.add('bgImage-animation');
-	setTimeout(() => bgImage.classList.remove('bgImage-animation'),1000);
-};
+	if (currentIndexImage <= 1) {
+		currentIndexImage = images.length + 1;
+	}
+	currentIndexImage--;
+	bgImage.src = `${images[currentIndexImage-1]}`;
+	bgImage.classList.add('bgImage-animation')
+	setTimeout(() => bgImage.classList.remove('bgImage-animation'),500)
+}
 const keyBtnClick = function(event) {
-	if (event.keyCode === leftArrow) {
+	if (event.keyCode === LEFT_ARROW_KEY) {
 		btnPrevClick()
-	};	
-	if (event.keyCode === rightArrow) {
+	}	
+	if (event.keyCode === RIGHT_ARROW_KEY) {
 		btnNextClick()
-	};
-};
+	}
+}
 btnNext.addEventListener('click', btnNextClick);
+
 document.addEventListener('keydown', keyBtnClick);
+
 btnPrev.addEventListener('click', btnPrevClick);
+
 document.addEventListener('keydown', keyBtnClick);
